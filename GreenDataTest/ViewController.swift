d4d892bd984486707ca6c4d86c826615e0c9b610
Vlasoft <vlasoft.company@gmail.com>
2020-11-06 01:31:52 +0500

    Initial Commit

---
+++ GreenDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GreenDataTest
+//
+//  Created by yaruncle on 06.11.2020.
+//  Copyright © 2020 yaruncle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
