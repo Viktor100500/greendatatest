e4e1c8fed5233ad1e9ed02c0e843146b59d331d8
Vlasoft <vlasoft.company@gmail.com>
2020-11-22 20:10:38 +0500

    version 1.2

---
+++ GreenDataTest/Model/ArrayExtension.swift
@@ -0,0 +1,23 @@
+//
+//  ArrayExtension.swift
+//  GreenDataTest
+//
+//  Created by yaruncle on 22.11.2020.
+//  Copyright © 2020 yaruncle. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+// Попытка написать кастомный аппенд
+extension Array where Element: EPerson {
+    
+//    func append(contentsOf: Array<Person>) -> Void {
+//        for i in contentsOf {
+//            var newItem: EPerson
+//            newItem = i.convertToEntity()
+//            Array.append(newItem)
+//        }
+//    }
+}
